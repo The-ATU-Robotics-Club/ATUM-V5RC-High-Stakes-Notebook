0bb167e54273672cc832a9c7b60a8cd9fa4ab69e
Braden Pierce <46367025+theguy20phi@users.noreply.github.com>
2025-01-21 03:20:22 -0600

    updated the start of game analysis, updated the brainstorming and early design section

--- software/code/files/impl/src/robotPrototype/disabledPrototype.cpp
+++
@@ -1,9 +0,0 @@
-#include "robotPrototype.hpp"
-
-namespace atum {
-RobotPrototype::RobotPrototype() : Robot{this} {
-    climbMotors.setBrakeMode(pros::v5::MotorBrake::brake);
-}
-
-void RobotPrototype::disabled() {}
-} // namespace atum
